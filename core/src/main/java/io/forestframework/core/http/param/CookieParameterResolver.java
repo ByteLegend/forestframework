9ad709e5bd0dc2026fb6b16a9ea688f3b68387c2
Bo Zhang <zhangbodut@gmail.com>
2020-10-25 20:09:13 +0800

    Support @Cookie for WebSocket handler

--- core/src/main/java/io/forestframework/core/http/param/CookieParameterResolver.java
+++ core/src/main/java/io/forestframework/core/http/param/CookieParameterResolver.java
@@ -1,13 +1,13 @@
 package io.forestframework.core.http.param;
 
-import io.forestframework.core.http.HttpContext;
+import io.forestframework.core.http.WebContext;
 import io.forestframework.core.http.routing.Routing;
 
 import static com.github.blindpirate.annotationmagic.AnnotationMagic.getOneAnnotationOnMethodParameterOrNull;
 
-public class CookieParameterResolver implements RoutingParameterResolver<HttpContext> {
+public class CookieParameterResolver implements RoutingParameterResolver<WebContext> {
     @Override
-    public Object resolveParameter(HttpContext context, Routing routing, int paramIndex) {
+    public Object resolveParameter(WebContext context, Routing routing, int paramIndex) {
         Cookie anno = getOneAnnotationOnMethodParameterOrNull(routing.getHandlerMethod(), paramIndex, Cookie.class);
         io.vertx.core.http.Cookie cookie = context.request().getCookie(anno.value());
         return cookie == null ? null : cookie.getValue();
